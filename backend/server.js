84a474136d9f5762d3bb13644850beb421fded63
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-04-13 21:35:01 +0530

    brought backend and frontend on the same route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRouter from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -11,7 +12,16 @@ app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use("/api/products", productRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
