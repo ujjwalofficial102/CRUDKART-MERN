2e80b5b5393e2f52ee4e0a9eab2adb155661b851
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-04-13 20:35:59 +0530

    minor changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Database connected");
   } catch (error) {
-    console.log(`dbERROR: ${error.message}`);
     process.exit(1);
   }
 };
